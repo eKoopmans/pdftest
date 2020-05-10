3fdbf14eec91fb8d6a2f6a292b32c875255b7824
Erik Koopmans <erik@erik-koopmans.com>
2020-05-10 21:29:26 +1000

    Add initial code and manual tester

---
+++ src/index.js
@@ -0,0 +1,61 @@
+import pdfjs from 'pdfjs-dist';
+// import {getDocument as pdfjsGetDocument} from 'pdfjs-dist';
+// pdfjs = require('pdfjs-dist');
+
+// console.log(pdfjs);
+// console.log(pdfjs.getDocument);
+
+// console.log(pdfjsGetDocument);
+console.log(pdfjs.getDocument);
+
+export const test1 = 'a';
+export const test2 = 'b';
+export const test3 = 'c';
+
+export const test = function (file) {
+// export default function (file) {
+    const isFile = file instanceof File;
+
+  if (isFile) {
+    var fileReader = new FileReader();
+    fileReader.onload = function(ev) {
+      console.log('event', ev);
+      // var data = new Uint8Array(fileReader.result);
+      var data = fileReader.result;
+      pdfjs.getDocument(data).then(function getPdfHelloWorld(pdf) {
+        //
+        // Fetch the first page
+        //
+        console.log('pdf', pdf)
+        pdf.getPage(1).then(function getPageHelloWorld(page) {
+          var scale = 1.5;
+          var viewport = page.getViewport(scale);
+
+          //
+          // Prepare canvas using PDF page dimensions
+          //
+          console.log('document', document);
+          // var canvas = document.getElementById('the-canvas');
+          var canvas = document.createElement('canvas');
+          var context = canvas.getContext('2d');
+          canvas.height = viewport.height;
+          canvas.width = viewport.width;
+
+          //
+          // Render PDF page into canvas context
+          //
+          var task = page.render({canvasContext: context, viewport: viewport})
+          task.promise.then(function(){
+            // console.log(canvas.toDataURL('image/jpeg'));
+            var img = document.createElement('img');
+            img.src = canvas.toDataURL('image/jpeg');
+            document.body.appendChild(img);
+          });
+        });
+      }, function(error){
+        console.log(error);
+      });
+    };
+    fileReader.readAsArrayBuffer(file);
+  }
+}
