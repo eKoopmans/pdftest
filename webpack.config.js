999aaf0dcfe90edaa6d918d3758940f7be044931
Erik Koopmans <erik@erik-koopmans.com>
2020-05-10 21:23:38 +1000

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const webpack = require('webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = env => {
+  const isDev = env === 'dev';
+  const mode = isDev ? 'development' : 'production';
+  const minStr = isDev ? '' : '.min';
+  const watch = isDev;
+
+  return {
+    mode: mode,
+    entry: './src/index.js',
+    watch: watch,
+    watchOptions: {
+      ignored: /node_modules/,
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: `main${minStr}.js`,
+      library: 'pdftest',
+      // libraryTarget: 'umd',
+      // libraryExport: 'default',
+      // umdNamedDefine: true,
+    },
+    plugins: [
+      new webpack.optimize.LimitChunkCountPlugin({
+        maxChunks: 1,
+      }),
+      new BundleAnalyzerPlugin({
+        analyzerMode: 'disabled',
+        generateStatsFile: true,
+        statsFilename: `stats${minStr}.json`,
+        statsOptions: { source: false },
+      }),
+    ],
+  };
+};
