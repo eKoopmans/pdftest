f9545c0d34020d66e147103361fe645a0453de06
Erik Koopmans <erik@erik-koopmans.com>
2020-10-03 21:02:11 +1000

    Split up compare logic

--- src/compare.js
+++ src/compare.js
@@ -1,6 +1,42 @@
 import pixelmatch from 'pixelmatch'
 import { getPdfObjects, getPageImages } from './read-pdf'
 
+async function comparePage(pdfObjects, page, settings) {
+  // Initialise the page match results.
+  let match = false
+  let numDiffPixels = null
+  let diffImg = null
+
+  // Get an image of the current page for both PDFs.
+  const [ pageImg1, pageImg2, error ] = await getPageImages(pdfObjects, page)
+
+  if (error) {
+    console.error(error)
+  } else {
+    // Create a canvas and run pixelmatch.
+    const diffCanvas = document.createElement('canvas')
+    diffImg = diffCanvas.getContext('2d').createImageData(pageImg1.width, pageImg1.height)
+    numDiffPixels = pixelmatch(pageImg1.data, pageImg2.data, diffImg.data, diffImg.width, diffImg.height, settings.pixelmatch)
+
+    // Determine whether the page matches according to failureThreshold.
+    if (settings.failureThresholdType === 'percent') {
+      const numPixels = pageImg1.width * pageImg1.height
+      const proportionDifferent = numDiffPixels / numPixels
+      match = proportionDifferent <= settings.failureThreshold
+    } else {
+      match = numDiffPixels <= settings.failureThreshold
+    }
+  }
+
+  // Build results for the current page.
+  return {
+    match: match,
+    pageImg: [pageImg1, pageImg2],
+    diffImg: diffImg,
+    numDiffPixels: numDiffPixels,
+  }
+}
+
 async function compare(pdf1, pdf2, settings) {
   // Set up default settings for pixelmatch.
   settings = {
@@ -21,40 +57,8 @@ async function compare(pdf1, pdf2, settings) {
   }
 
   for (var i=0; i<result.nPages; i++) {
-    // Initialise the page match results.
-    let match = false
-    let numDiffPixels = null
-    let diffImg = null
-
-    // Get an image of the current page for both PDFs.
-    const [ pageImg1, pageImg2, error ] = await getPageImages(pdfObjects, i)
-
-    if (error) {
-      console.error(error)
-    } else {
-      // Create a canvas and run pixelmatch.
-      const diffCanvas = document.createElement('canvas')
-      diffImg = diffCanvas.getContext('2d').createImageData(pageImg1.width, pageImg1.height)
-      numDiffPixels = pixelmatch(pageImg1.data, pageImg2.data, diffImg.data, diffImg.width, diffImg.height, settings.pixelmatch)
-
-      // Determine whether the page matches according to failureThreshold.
-      if (settings.failureThresholdType === 'percent') {
-        const numPixels = pageImg1.width * pageImg1.height
-        const proportionDifferent = numDiffPixels / numPixels
-        match = proportionDifferent <= settings.failureThreshold
-      } else {
-        match = numDiffPixels <= settings.failureThreshold
-      }
-    }
-
-    // Build results for the current page.
-    result.pageResults[i] = {
-      match: match,
-      pageImg: [pageImg1, pageImg2],
-      diffImg: diffImg,
-      numDiffPixels: numDiffPixels,
-    }
-    result.match = result.match && match
+    result.pageResults[i] = comparePage(pdfObjects, i, settings)
+    result.match = result.match && result.pageResults[i].match
   }
 
   return result
