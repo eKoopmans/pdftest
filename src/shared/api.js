05116e0f2e7cdbd0032c041ea8816f1259d97ae1
Erik Koopmans <erik@erik-koopmans.com>
2021-01-31 21:04:52 +1100

    Allow customisable returnType on api.get

--- src/shared/api.js
+++ src/shared/api.js
@@ -10,14 +10,19 @@ export function getConnection() {
   return state.connection
 }
 
-export async function get(filepath) {
+/**
+ * Get a PDF file from the server.
+ * @param {string} filepath Path of the target file.
+ * @param {string} [returnType='arrayBuffer'] One of 'arrayBuffer' or 'blob' (see fetch API).
+ */
+export async function get(filepath, returnType='arrayBuffer') {
   if (!state.connection) {
     return console.error('pdftest.api.get: Not connected to a server. Use api.connect(url) first.')
   }
 
   try {
     const response = await fetch(`${state.connection}/${filepath}`)
-    return response.ok ? await response.arrayBuffer() : undefined
+    return response.ok ? await response[returnType]() : undefined
   } catch (e) {
     return undefined
   }
