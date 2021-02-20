0f15dad945f05207a307873f464c5620cdc77f2b
Erik Koopmans <erik@erik-koopmans.com>
2021-02-21 01:05:46 +1100

    Add ability to start/stop server without blocking

--- bin/pdftest.js
+++ bin/pdftest.js
@@ -3,16 +3,27 @@
 const { program } = require('commander')
 const esrequire = require('esm')(module)
 const fs = require('fs')
+const path = require('path')
 
 const pkg = require('../package.json')
 const pdftestServer = esrequire('../src/server')
 const pdftestClient = esrequire('../src/client')
+
+const datastore = path.resolve(__dirname, 'data.json');
 
 program.version(pkg.version, '-v, --version')
 program
   .command('serve [port] [root]')
   .description('Serve PDF files')
   .action(pdftestServer.serve)
+program
+  .command('start [port] [root]')
+  .description('Start a PDF server (non-blocking)')
+  .action(start)
+program
+  .command('stop [port]')
+  .description('Stop a PDF server created by pdftest start')
+  .action(stop)
 program
   .command('compare <file1> <file2>')
   .description('Compare two PDF files')
@@ -30,4 +41,34 @@ async function compare(file1, file2) {
   })
   const res = await pdftestClient.compare(data[0], data[1])
   console.log(res)
+}
+
+function start(port, root) {
+  const { spawn } = require('child_process')
+  const args = root ? [port, root] : port ? [port] : []
+
+  stop(port)
+
+  console.log(`pdftest: Starting port ${port}`)
+  const serverProcess = spawn('node', [__filename, 'serve', ...args], {
+    stdio: [process.stdin, process.stdout, process.stderr],
+    detached: true,
+  })
+  serverProcess.unref()
+
+  const data = fs.existsSync(datastore) ? JSON.parse(fs.readFileSync(datastore) || '{}') : {}
+  data[port] = { pid: serverProcess.pid }
+  fs.writeFileSync(datastore, JSON.stringify(data))
+}
+
+function stop(port) {
+  const data = fs.existsSync(datastore) ? JSON.parse(fs.readFileSync(datastore) || '{}') : {}
+
+  if (data[port]) {
+    console.log(`pdftest: Stopping port ${port}`)
+    try { process.kill(data[port].pid) } catch {}
+    delete data[port]
+  }
+
+  fs.writeFileSync(datastore, JSON.stringify(data))
 }
