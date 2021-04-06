0999ae8858573261e652cc7b240558a9a74f69c5
Erik Koopmans <erik@erik-koopmans.com>
2021-04-06 23:40:52 +1000

    Update chai snapshot messaging

--- src/frameworks/chai-pdftest.js
+++ src/frameworks/chai-pdftest.js
@@ -3,5 +3,5 @@
 chai.Assertion.addMethod('matchPdfSnapshot', async function (refname, settings) {
   const obj = this._obj
   const result = await pdftest.compareToSnapshot(obj, refname, settings)
-  this.assert(result.match === true, 'PDF does not match snapshot.')
+  this.assert(result.match === true, `PDF does not match stored snapshot ${refname}.`, `PDF matches stored snapshot ${refname}.`)
 })
